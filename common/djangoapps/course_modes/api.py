826fb3d2d6364ffa2ee68a5220bc3522bde7e3b1
Matt Tuchfarber <mtuchfarber@edx.org>
2020-08-24 11:20:09 -0400

    Fix

--- common/djangoapps/course_modes/api.py
+++ common/djangoapps/course_modes/api.py
@@ -1,7 +1,7 @@
 from typing import List
 from opaque_keys.edx.keys import CourseKey
 
-from course_modes.models import _CourseMode
+from course_modes.models import CourseMode as _CourseMode
 
 
 def get_paid_modes_for_course(course_run_id: CourseKey) -> List[str]:
